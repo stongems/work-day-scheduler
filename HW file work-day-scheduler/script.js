efdf701552688678a488a31a1d9a7b9bda4a5c3b
St.Onge <stonge.ms@gmail.com>
2021-06-11 14:36:53 -0700

    Updated the Link in README

--- HW file work-day-scheduler/script.js
+++ HW file work-day-scheduler/script.js
@@ -1,30 +1,38 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+// ********* BROKEN SAVE FUNCTION************//
 
-console.log("test")
+
+
+
+console.log("test");
 //Using Momentjs is how we are getting the current hour
 const currentHour = +moment().format("H");
 console.log(currentHour);
 //We are slecting all elements with the class of time-block and executing a function for each one
-$(".time-block").each(function(){
-    //"this" is refering to .time-block 
-    //".attr" gets the element id
-    //".split" used to split a string into an array
-    //".pop" Removes the last element of the newly created array
-const hour = $(this).attr("id").split("-")
-console.log(hour)
+// $(".time-block").each(function(){
+//"this" is refering to .time-block
+//".attr" gets the element id
+//".split" used to split a string into an array
+//     //".pop" Removes the last element of the newly created array
+// const hour = $(this).attr("id").split("-")
 
 
+$(".time-block").each(function () {
+  const hour = $(this).attr("id").split("-").pop();
+  console.log(".time-block");
+  console.log(this);
+  
+  if (currentHour === hour) {
+    //"present" css class name
+    $(this).addClass("present");
+  } else if (currentHour < hour) {
+    //future css class name
+    $(this).addClass("future");
+  } else { //past css class name
+    $(this).addClass("past");   
+  }
+  
 });
+
+// ^^^^^^^^^THE TEXT WILL NOT SAVE POST REFRESH^^^^^^^^^^^^
+// JS need a DOM traversal to and from the text box********
 
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+
+
+HOMEWORK 
 # work-day-scheduler
+
+## Proposal/Requirements
+[  ] GIVEN I am using a daily planner to create a schedule
+[  ] WHEN I open the planner
+[  ] THEN the current day is displayed at the top of the calendar
+[  ] WHEN I scroll down
+[  ] THEN I am presented with timeblocks for standard business hours
+[  ] WHEN I view the timeblocks for that day
+[  ] THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+[  ] WHEN I click into a timeblock
+[  ] THEN I can enter an event
+[  ] WHEN I click the save button for that timeblock
+[  ] THEN the text for that event is saved in local storage
+[  ] WHEN I refresh the page
+[  ] THEN the saved events persist
+
+## Design
+![image](./assets/img/work-day-scheduler.png)
+
+## Links
+[REPOSITORY](https://github.com/stongems)
+```
+https://github.com/stongems
+```
+
+[DEPLOYED](link.com)
+```
+link.com
+```
